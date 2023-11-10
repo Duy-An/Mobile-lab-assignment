56b7c1f26e77b8aa79ee1d7ad97a98bd4f3a6979
Duy-An <119985391+Duy-An@users.noreply.github.com>
2023-11-09 20:53:09 -0700

    lab3-upload

--- IncredibleToDoList/App.jsx
+++ IncredibleToDoList/App.jsx
@@ -1,20 +1,24 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
-import ToDoList from './ToDoList'; // Assuming ToDoList.jsx is in the same directory
-import ToDoForm from './ToDoForm'; // Assuming ToDoForm.jsx is in the same directory
+"use client";
+
+import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native';
+import ToDoList from './ToDoList';
+import ToDoForm from './ToDoForm';
 
 function App() {
+  const [tasks] = useState([
+    'Do laundry',
+    'Go to gym',
+    'Walk dog'
+  ]);
+
   return (
     <SafeAreaView>
-      <ToDoList />
+      <ToDoList tasks={tasks} />
       <ToDoForm />
     </SafeAreaView>
   );
 }
 
-// The styles may need to be adjusted accordingly if they are used in the child components.
-const styles = StyleSheet.create({
-  // ... styles that are used within App component itself
-});
-
 export default App;
+
